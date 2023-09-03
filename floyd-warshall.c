49e8f18dce3ecb26341b20fce851d314bd181026
Tom Karlsson <tomk4@kth.se>
2023-09-03 21:09:28 +0200

    Vanilla implementation works with simple testgraph

--- floyd-warshall.c
+++ floyd-warshall.c
@@ -38,8 +38,13 @@ typedef struct Edge {
     int v1,v2,w;
     struct Edge *next;
 } Edge_t;
-void printGraph(Edge_t *head);
-void freeGraph(Edge_t *head);
+
+void    printGraph(Edge_t *head);
+void    freeGraph(Edge_t *head);
+int     getWeight(int u, int v);
+void    setWeight(int u, int v, int w);
+
+
 
 void* loadGraph(int edges){
     printf("Edges: %d\n", edges);
@@ -105,13 +110,65 @@ void* loadGraph(int edges){
     return head;
 }
 
-/* TODO */
 int initArray(Edge_t *head){
     if(! (maxValueVertice > 0))
         return 0;
-    //vertices_2D_array = malloc(edges*edges*sizeof(int));
+
+    vertices_2D_array = malloc(maxValueVertice*maxValueVertice*sizeof(int));
+
+    /* Set all weights to infinity (max value of 32 bit integer)*/
+    for(int i = 0; i < maxValueVertice; i++){
+        for(int j = 0; j < maxValueVertice; j++){
+            vertices_2D_array[i * maxValueVertice + j] = INT32_MAX;
+        }
+    }
+    /* Set weight/distance of (v,v) to 0*/
+    for(int i = 0; i < maxValueVertice; i++){
+        vertices_2D_array[i * maxValueVertice + i] = 0;
+    }
+    /* Transfer initial weights from graph*/
+    Edge_t *tmp = head;
+    while(tmp->next != NULL){
+        if(getWeight(tmp->v1,tmp->v2) > tmp->w){
+            setWeight(tmp->v1,tmp->v2,tmp->w);
+        }
+        tmp = tmp->next;
+    }
 
     return 1;
+}
+
+/* Runs the Floyd-Warshall algorithm on the 2D-array. 
+Returns 0 (false) if a negative cycle is detected.
+Otherwise return 1 if successfull execution */
+int floydWarshall(){
+    for(int k = 1; k <= maxValueVertice; k++)
+        for(int i = 1; i <= maxValueVertice; i++)
+            for(int j = 1; j <= maxValueVertice; j++){
+                if(getWeight(i,k) < INT32_MAX && getWeight(k,j) < INT32_MAX){
+                    int weightThroughK = getWeight(i,k) + getWeight(k,j);
+                    /* Check for negative cycle */
+                    if(i == j && weightThroughK < 0)
+                        return 0; 
+                    if(getWeight(i,j) > weightThroughK){
+                        /* Found a "shorter" path from i to j through k*/
+                        // printf("(%d,%d) previous w: %d, new w: %d through %d \n", i,j,getWeight(i,j), weightThroughK,k);
+                        setWeight(i,j, weightThroughK);
+                    }
+                }
+            }
+    return 1;
+}
+
+int getWeight(int u, int v){
+    /* Subtract 1 to get 0-index */
+    return vertices_2D_array[(u-1) * maxValueVertice + (v-1)];
+}
+
+void setWeight(int u, int v, int w){
+    /* Subtract 1 to get 0-index */
+    vertices_2D_array[(u-1) * maxValueVertice + (v-1)] = w;
+    // vertices_2D_array[(v-1) * maxValueVertice + (u-1)] = w;
 }
 
 void printGraph(Edge_t *head){
@@ -124,6 +181,51 @@ void printGraph(Edge_t *head){
     printf("All edges printed.\n");
 }
 
+/* Function for visually printing out the 2D-array with vertices and weights */
+void printArray(){
+    printf("  |1");
+    for(int i = 1; i < maxValueVertice; i++)
+        printf("    |%d", i+1);
+    printf("   |\n  |____");
+    for(int i = 1; i < maxValueVertice; i++)
+        printf("_|____");
+    printf("|\n");
+    for(int i = 0; i < maxValueVertice; i++){
+        printf("%d", i+1);
+        for(int j = 0; j < maxValueVertice; j++){
+            int val = vertices_2D_array[i*maxValueVertice + j];
+            int sign = 2;
+            if(val < 0){
+                val = (~val)+1;
+                sign = 1;
+            }
+            char space[sign + 1];
+            space[sign] = 0;
+            while(sign-- > 0)
+                space[sign] = ' ';
+
+            if(val < 100){
+                if(val < 10){
+                    printf(" |%d%s ", vertices_2D_array[i*maxValueVertice + j], space);
+
+                }
+                else{
+                    printf(" |%d%s", vertices_2D_array[i*maxValueVertice + j], space);
+
+                }
+            }
+            else
+                printf(" |inf ");
+        }
+        printf("|\n");
+    }
+    printf("  |____");
+    for(int i = 1; i < maxValueVertice; i++)
+        printf("______");
+    printf("|\n\n");
+
+}
+
 void freeGraph(Edge_t *head){
     Edge_t *tmp;
     while(head->next != NULL){
@@ -132,6 +234,10 @@ void freeGraph(Edge_t *head){
         free(tmp);
     }
     printf("Graph memory allocation is freed.\n");
+}
+
+void freeArray(){
+    free(vertices_2D_array);
 }
 
 int main(){
@@ -149,6 +255,10 @@ int main(){
     printGraph(head);
     initArray(head);
     freeGraph(head);
+    printArray();
+    floydWarshall();
+    printArray();
+    freeArray();
 
     return 1;
 }
